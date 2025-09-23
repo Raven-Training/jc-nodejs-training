59231e8841927702541de3579679eb34ec2f4502
juan camacho <juan.camacho@raven.inc>
2025-09-23 14:37:22 -0500

    style: format code for consistency in AutoMigration file

--- src/migrations/1758654551620-AutoMigration.ts
+++ src/migrations/1758654551620-AutoMigration.ts
@@ -1,18 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AutoMigration1758654551620 implements MigrationInterface {
-    name = 'AutoMigration1758654551620'
+  name = 'AutoMigration1758654551620';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastName"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "last_name" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "password" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastName"`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "last_name" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "password" character varying NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "last_name"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastName" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "last_name"`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "lastName" character varying NOT NULL`);
+  }
 }

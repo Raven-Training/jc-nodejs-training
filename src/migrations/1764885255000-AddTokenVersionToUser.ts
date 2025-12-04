a5014f32bc2f31154259ef65614b8b716f4ece7d
juan camacho <juan.camacho@raven.inc>
2025-12-04 17:00:18 -0500

    feat: Add token version column to User entity and migration

---
+++ src/migrations/1764885255000-AddTokenVersionToUser.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddTokenVersionToUser1764885255000 implements MigrationInterface {
+  name = 'AddTokenVersionToUser1764885255000';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "token_version" integer NOT NULL DEFAULT 0`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "token_version"`);
+  }
+}

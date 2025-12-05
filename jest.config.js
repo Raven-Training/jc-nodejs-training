191023ff3a14cdfcdeb0b4d0060e57e44af2ff6a
juan camacho <juan.camacho@raven.inc>
2025-12-05 10:30:08 -0500

    feat: Add token version handling and session invalidation functionality

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   verbose: true,
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   testPathIgnorePatterns: ['<rootDir>/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   transform: {
     ...tsJestTransformCfg,
   },
